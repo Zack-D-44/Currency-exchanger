a122d0fd4df629ad08c73f669cfe34d81e3349b1
zackd44 <zackdavison1@outlook.com>
2024-02-15 21:13:09 +0000

    Add project files.

---
+++ Currency exchanger/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace Currency_exchanger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LblCurrencyExchangeFrom = new System.Windows.Forms.Label();
+            this.CboCurrencyChangeFrom = new System.Windows.Forms.ComboBox();
+            this.BtnExchange = new System.Windows.Forms.Button();
+            this.TxtAmountToChangeFrom = new System.Windows.Forms.TextBox();
+            this.TxtExchangedValue = new System.Windows.Forms.TextBox();
+            this.CboCurrencyToExchangeTo = new System.Windows.Forms.ComboBox();
+            this.LblEnterExchangeCurrency = new System.Windows.Forms.Label();
+            this.LblExchangedValue = new System.Windows.Forms.Label();
+            this.BtnSwap = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LblCurrencyExchangeFrom
+            // 
+            this.LblCurrencyExchangeFrom.AutoSize = true;
+            this.LblCurrencyExchangeFrom.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblCurrencyExchangeFrom.Location = new System.Drawing.Point(47, 39);
+            this.LblCurrencyExchangeFrom.Name = "LblCurrencyExchangeFrom";
+            this.LblCurrencyExchangeFrom.Size = new System.Drawing.Size(247, 19);
+            this.LblCurrencyExchangeFrom.TabIndex = 0;
+            this.LblCurrencyExchangeFrom.Text = "Enter Currency to Change from";
+            // 
+            // CboCurrencyChangeFrom
+            // 
+            this.CboCurrencyChangeFrom.FormattingEnabled = true;
+            this.CboCurrencyChangeFrom.Items.AddRange(new object[] {
+            "AUD",
+            "BGN",
+            "BRL",
+            "CAD",
+            "CHF",
+            "CNY",
+            "CZK",
+            "DKK",
+            "EUR",
+            "GBP",
+            "HKD",
+            "HRK",
+            "HUF",
+            "IDR",
+            "ILS",
+            "INR",
+            "ISK",
+            "JPY",
+            "KRW",
+            "MXN",
+            "MYR",
+            "NOK",
+            "NZD",
+            "PHP",
+            "PLN",
+            "RON",
+            "RUB",
+            "SEK",
+            "SGD",
+            "THB",
+            "TRY",
+            "USD",
+            "ZAR"});
+            this.CboCurrencyChangeFrom.Location = new System.Drawing.Point(51, 62);
+            this.CboCurrencyChangeFrom.Name = "CboCurrencyChangeFrom";
+            this.CboCurrencyChangeFrom.Size = new System.Drawing.Size(54, 21);
+            this.CboCurrencyChangeFrom.TabIndex = 1;
+            // 
+            // BtnExchange
+            // 
+            this.BtnExchange.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnExchange.Location = new System.Drawing.Point(331, 113);
+            this.BtnExchange.Name = "BtnExchange";
+            this.BtnExchange.Size = new System.Drawing.Size(97, 30);
+            this.BtnExchange.TabIndex = 2;
+            this.BtnExchange.Text = "Exchange ";
+            this.BtnExchange.UseVisualStyleBackColor = true;
+            this.BtnExchange.Click += new System.EventHandler(this.BtnExchange_Click);
+            // 
+            // TxtAmountToChangeFrom
+            // 
+            this.TxtAmountToChangeFrom.Location = new System.Drawing.Point(111, 62);
+            this.TxtAmountToChangeFrom.Name = "TxtAmountToChangeFrom";
+            this.TxtAmountToChangeFrom.Size = new System.Drawing.Size(138, 20);
+            this.TxtAmountToChangeFrom.TabIndex = 3;
+            // 
+            // TxtExchangedValue
+            // 
+            this.TxtExchangedValue.Enabled = false;
+            this.TxtExchangedValue.Location = new System.Drawing.Point(544, 135);
+            this.TxtExchangedValue.Name = "TxtExchangedValue";
+            this.TxtExchangedValue.Size = new System.Drawing.Size(183, 20);
+            this.TxtExchangedValue.TabIndex = 6;
+            // 
+            // CboCurrencyToExchangeTo
+            // 
+            this.CboCurrencyToExchangeTo.FormattingEnabled = true;
+            this.CboCurrencyToExchangeTo.Items.AddRange(new object[] {
+            "AUD",
+            "BGN",
+            "BRL",
+            "CAD",
+            "CHF",
+            "CNY",
+            "CZK",
+            "DKK",
+            "EUR",
+            "GBP",
+            "HKD",
+            "HRK",
+            "HUF",
+            "IDR",
+            "ILS",
+            "INR",
+            "ISK",
+            "JPY",
+            "KRW",
+            "MXN",
+            "MYR",
+            "NOK",
+            "NZD",
+            "PHP",
+            "PLN",
+            "RON",
+            "RUB",
+            "SEK",
+            "SGD",
+            "THB",
+            "TRY",
+            "USD",
+            "ZAR"});
+            this.CboCurrencyToExchangeTo.Location = new System.Drawing.Point(544, 62);
+            this.CboCurrencyToExchangeTo.Name = "CboCurrencyToExchangeTo";
+            this.CboCurrencyToExchangeTo.Size = new System.Drawing.Size(192, 21);
+            this.CboCurrencyToExchangeTo.TabIndex = 5;
+            // 
+            // LblEnterExchangeCurrency
+            // 
+            this.LblEnterExchangeCurrency.AutoSize = true;
+            this.LblEnterExchangeCurrency.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblEnterExchangeCurrency.Location = new System.Drawing.Point(540, 39);
+            this.LblEnterExchangeCurrency.Name = "LblEnterExchangeCurrency";
+            this.LblEnterExchangeCurrency.Size = new System.Drawing.Size(242, 19);
+            this.LblEnterExchangeCurrency.TabIndex = 4;
+            this.LblEnterExchangeCurrency.Text = "Enter Currency to exchange to";
+            // 
+            // LblExchangedValue
+            // 
+            this.LblExchangedValue.AutoSize = true;
+            this.LblExchangedValue.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblExchangedValue.Location = new System.Drawing.Point(562, 113);
+            this.LblExchangedValue.Name = "LblExchangedValue";
+            this.LblExchangedValue.Size = new System.Drawing.Size(145, 19);
+            this.LblExchangedValue.TabIndex = 7;
+            this.LblExchangedValue.Text = "Exchanged value ";
+            // 
+            // BtnSwap
+            // 
+            this.BtnSwap.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnSwap.Location = new System.Drawing.Point(331, 78);
+            this.BtnSwap.Name = "BtnSwap";
+            this.BtnSwap.Size = new System.Drawing.Size(97, 29);
+            this.BtnSwap.TabIndex = 8;
+            this.BtnSwap.Text = "Swap";
+            this.BtnSwap.UseVisualStyleBackColor = true;
+            this.BtnSwap.Click += new System.EventHandler(this.BtnSwap_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1511, 934);
+            this.Controls.Add(this.BtnSwap);
+            this.Controls.Add(this.LblExchangedValue);
+            this.Controls.Add(this.TxtExchangedValue);
+            this.Controls.Add(this.CboCurrencyToExchangeTo);
+            this.Controls.Add(this.LblEnterExchangeCurrency);
+            this.Controls.Add(this.TxtAmountToChangeFrom);
+            this.Controls.Add(this.BtnExchange);
+            this.Controls.Add(this.CboCurrencyChangeFrom);
+            this.Controls.Add(this.LblCurrencyExchangeFrom);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LblCurrencyExchangeFrom;
+        private System.Windows.Forms.ComboBox CboCurrencyChangeFrom;
+        private System.Windows.Forms.Button BtnExchange;
+        private System.Windows.Forms.TextBox TxtAmountToChangeFrom;
+        private System.Windows.Forms.TextBox TxtExchangedValue;
+        private System.Windows.Forms.ComboBox CboCurrencyToExchangeTo;
+        private System.Windows.Forms.Label LblEnterExchangeCurrency;
+        private System.Windows.Forms.Label LblExchangedValue;
+        private System.Windows.Forms.Button BtnSwap;
+    }
+}
+
---
+++ Currency exchanger/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+
+
+
+namespace Currency_exchanger
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void BtnExchange_Click(object sender, EventArgs e)
+        {   
+            //declaring variables
+            string apiKey = "api key";
+            string currencyToExchangeFrom = CboCurrencyChangeFrom.Text;
+            string currencyToExchangeTo = CboCurrencyToExchangeTo.Text;
+            float valueToExchangeFrom = 0.0f;
+
+            //checking if value to exchange from is a currnecy value if not message box appears
+            try
+            {
+                valueToExchangeFrom = float.Parse(TxtAmountToChangeFrom.Text);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("The value to exchange should be entered and it should be a currency value", "Error");
+                return;
+            }
+
+
+            //checking that currency to exchange from is not left empty and is a value from the dropdown list if one of these is false error message appears
+            if (string.IsNullOrWhiteSpace(currencyToExchangeFrom) || !CboCurrencyChangeFrom.Items.Contains(currencyToExchangeFrom)) 
+            {
+                MessageBox.Show("You must enter a currency to exchange from, that is from the dropdown menu", "Error");
+                return;
+            }
+
+            //checking that currency to exchange to is not left empty and is a value from the dropdown list if one of these is false error message appears
+            if (string.IsNullOrWhiteSpace(currencyToExchangeTo) || !CboCurrencyToExchangeTo.Text.Contains(currencyToExchangeTo))
+            {
+                
+                MessageBox.Show("You must enter a currency to exhange to, that is from the dropdown menu", "Error");
+                return;
+            }
+            
+            else
+            {
+                //creating http client
+                using (HttpClient client = new HttpClient())
+                {
+
+                    //creating API URL
+                    string url = $"https://api.freecurrencyapi.com/v1/latest?apikey={apiKey}&currencies={currencyToExchangeTo}&base_currency={currencyToExchangeFrom}";
+
+                    //making asynchronous GET request
+                    HttpResponseMessage response = await client.GetAsync(url);
+                    //ensuring call successful
+                    response.EnsureSuccessStatusCode();
+                    
+                    //deserialising response as instance of class
+                    string responseBody = await response.Content.ReadAsStringAsync();   
+                    var exchangeResult = JsonSerializer.Deserialize<ExchangeResult>(responseBody);
+                    
+                    //assigning exchange rate to variable
+                    float exchangeRate = exchangeResult.data[currencyToExchangeTo];
+
+                    //performing calulation to work out exchange rate
+                    float convertedValue = valueToExchangeFrom * exchangeRate;
+
+                    //rounding value to two decimal places and inserting value into text box
+                    convertedValue = (float)Math.Round(convertedValue, 2);
+                    TxtExchangedValue.Text = convertedValue.ToString();
+
+                    
+
+                    
+                }
+
+                
+
+            }
+                
+               
+            }
+
+        private void BtnSwap_Click(object sender, EventArgs e)
+        {   
+            //assigning currency from and two variables values in corresponding combo boxes
+            string currencyFrom = CboCurrencyChangeFrom.Text;
+            string currencyTo = CboCurrencyToExchangeTo.Text;
+
+            //assigning the boxes the text of the other box
+            CboCurrencyChangeFrom.Text = currencyTo;
+            CboCurrencyToExchangeTo.Text = currencyFrom;
+        }
+    }
+
+    public class ExchangeResult
+        {
+            //gets or sets a dictionary containg the exchange rates 
+            public Dictionary<string, float> data { get; set; }
+
+        }
+    }
+
